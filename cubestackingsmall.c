d3bbf64d6ca49960dca1b56ee64e97c37af1018f
DohaBritish <59561784+DohaBritish@users.noreply.github.com>
2020-01-13 17:34:12 +0300

    Update cubestackingsmall.c

--- cubestackingsmall.c
+++ cubestackingsmall.c
@@ -5,7 +5,7 @@ int main()
     enable_servos();
     set_servo_position(3,1400);//claw open 
     msleep(500);
-    set_servo_position(0,1100);// claw down
+    set_servo_position(0,1850);// claw down
     msleep(500);
     disable_servos();
     clear_motor_position_counter(0);
@@ -31,11 +31,11 @@ int main()
     msleep(2000);
     ao();
     enable_servos();
-    set_servo_position(0,1200);//claw down 
+    set_servo_position(0,1950);//claw down 
     msleep(1000);
     set_servo_position(3,1300);//open claw
     msleep(500);
-    set_servo_position(0,900);//claw up
+    set_servo_position(0,1400);//claw up
     cmpc(0);
     while (gmpc(0) >= -1800)
     {
